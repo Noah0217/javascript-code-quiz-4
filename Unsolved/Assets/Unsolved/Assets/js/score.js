c2c05b4538aaf907cc916d65ff4d30710ba4e3bd
Noah0217 <84366215+Noah0217@users.noreply.github.com>
2021-07-15 15:01:11 -0600

    Add files via upload

---
+++ Unsolved/Assets/Unsolved/Assets/js/score.js
@@ -0,0 +1,21 @@
+// HighScores
+
+function printHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+     highscores.sort(function(a, b) {
+         return b.score - a.score;
+     });
+     highscores.forEach(function(score) {
+         var litag = document.createElement("li");
+         liTag.textContent = score.initials + " - " + score.score;
+         var olEl = document.getElementById("highscores");
+         olEl.appendChild(liTag);
+     });
+}
+
+function clearHighscores() {
+    window.localStorage.storage.removeItem("highscores")
+    window.location.reload();
+}
+document.getElementById("clear").onclick = clearHighscores;
+printHighscores();
---
+++ Unsolved/Assets/Unsolved/Assets/js/script.js
@@ -0,0 +1,125 @@
+//Quiz Timer
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerID;
+
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+//Sfx
+var sfxRight = new Audio("assets/unsolved/sfx/correct.wav")
+var sfxWrong = new Audio("assets/unsolved/sfx/incorrect.wav")
+
+function startQuiz() {
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    getQuestions();
+    startQuiz();
+
+}
+  
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionsIndex];
+    var titleEl = document.getElementById("questions-title");
+    choicesEl.innerHtml = "";
+    currentQuestion.choices.gotEach(function(choice, i) {
+        var choiceNode = document.getElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+        choiceNode.textContent = i + 1 + ". " + choice;
+        choiceNode.onclick = questionClick;
+        ChoicesEl.appendChild(choiceNode);
+    });
+}
+
+function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+time -= 15;
+
+if (time < 0) {
+time = 0;
+}
+
+timerEl.textContent = time;
+sfxWrong.play();
+feedbackEl.textContent = "Wrong!";
+    } else {
+        sfxRight.play();
+        feedbackEl.textContent = "Correct!";
+    }
+
+   feedbackEl.setAttribute("class", "feedback");
+   setTimeout(function() {
+       feedbackEl.setAttribute("class", "feedback hide");
+   }, 1000);
+
+   currentQuestionsIndex++;
+
+   if (currentQuestionsIndex === questions.length) {
+       quizEnd();
+   } else {
+       getQuestion();
+   }
+   }
+
+function quizEnd() {
+    clearInterval(timerId);
+
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+
+    questionsEl.setAttribute("class", "hide");
+
+}
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+
+    if (time <= 0) {
+        quizEnd();
+
+    }
+}
+
+function saveHighscore() {
+    var initials = initialsEl.value.trim();
+
+    if (initials !=="") {
+        var highscores = 
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        var newScore = {
+            score: time,
+            initials: initials
+        };
+
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        window.location.href = "score.html";
+    }
+}
+
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+}
+
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
